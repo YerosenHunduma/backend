c8a6dde55ba05140cc0b88357c6ce5c6aa4c155c
Yordanos tibebu <yordanostibebu5@gmail.com>
2024-04-11 02:38:57 -0700

    Changed the cloudinary config

--- config/cloudinaryConfig.js
+++ config/cloudinaryConfig.js
@@ -4,9 +4,9 @@ import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
 cloudinary.config({
-  cloud_name: "dm94aeunk",
-  api_key: "972997294255268",
-  api_secret: "NePK4_n2_dVJPfwENJiBcbcNDJ0",
+  cloud_name: process.env.cloud_name,
+  api_key: process.env.api_key,
+  api_secret: process.env.api_secret,
 });
 
 const uploadTocloudinary = async (localFilePath) => {
