3c340727ab8b7f65d6fba263cbb68bd5ee4688f7
YerosenHunduma <yerosenhunduma@gmail.com>
2024-04-11 11:52:09 +0300

    refactoring passport local strategy

--- config/passport/passport-local.js
+++ config/passport/passport-local.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 import { Strategy as LocalStartegy } from "passport-local";
 import User from "../../models/user.model.js";
+import Broker from "../../models/broker.model.js";
 
 const customFields = {
   usernameField: "email",
@@ -8,16 +9,35 @@ const customFields = {
 };
 
 const AuthenticatedUser = async (email, password, done) => {
+  console.log(email, password);
   try {
-    const user = await User.findOne({ email });
-    if (!user) return done(null, false, { message: "User not found" });
-    const isMathch = bcrypt.compare(password, user.password);
-    if (isMathch)
-      return done(null, user, { message: "signed in successfully" });
-    else
-      return done(null, false, {
-        message: "Either the Email or password you entered is incorrect",
-      });
+    let user = await User.findOne({ email });
+    if (!user) {
+      user = await Broker.findOne({ email });
+    }
+    if (user) {
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (isMatch) {
+        if (user instanceof Broker && !user.isApproved) {
+          return done(null, false, {
+            message: "Your account is not approved yet",
+          });
+        }
+        if (!user.active) {
+          return done(null, false, {
+            message:
+              "Your account is deactivated, please contact your administrator for more information",
+          });
+        }
+        return done(null, user, { message: "Signed in successfully" });
+      } else {
+        return done(null, false, {
+          message: "Either the Email or password you entered is incorrect",
+        });
+      }
+    } else {
+      return done(null, false, { message: "User not found" });
+    }
   } catch (error) {
     done(error);
   }
