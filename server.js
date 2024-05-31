349efc63210f07c13df3f9bbad3e12e0cf37834a
YerosenHunduma <yerosenhunduma@gmail.com>
2024-06-01 01:08:11 +0300

    configure cors in a way it accept cookies from any client

--- server.js
+++ server.js
@@ -18,7 +18,12 @@ process.on("uncaughtException", (error) => {
 
 const app = express();
 
-app.use(cors({ origin: "*", credentials: true }));
+app.use(
+  cors({
+    origin: "https://asset-marketsquare-react-jsou.onrender.com",
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
