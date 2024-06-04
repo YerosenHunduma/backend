a56a5981bfa9c5312bf44be5e0f3ffa7d1d4ac0f
YerosenHunduma <yerosenhunduma@gmail.com>
2024-06-04 22:49:04 +0300

    fix cors policy problem

--- server.js
+++ server.js
@@ -18,7 +18,12 @@ process.on("uncaughtException", (error) => {
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
